29295f10e5ff63ad4eabeba96c3961b97843aa82
lizirong <ivanbear@163.com>
2023-10-18 10:50:04 +0800

    mayavi contour3d

--- genModelChest.py
+++ genModelChest.py
@@ -42,3 +42,21 @@ mlab.show()
 #cv2.waitKey(0)
 #plt.imshow(temp[0])
 #plt.show()
+'''
+def RodriguesRotate(v:np.ndarray,u:np.ndarray,theta:float)->np.ndarray:
+    '''向量v绕向量u旋转角度θ,得到新的向量P_new
+    罗德里格斯旋转公式:v' = vcosθ + (u×v)sinθ + (u·v)u(1-cosθ)
+
+    args:
+        v:向量,维度为(3,)
+        u:作为旋转轴的向量,维度为(3,)
+        theta:旋转角度θ,此处默认为角度值
+    returns:
+        v_new:旋转后得到的向量,维度为(3,)
+    '''
+    u = u/np.linalg.norm(u) # 计算单位向量
+    sin_theta = np.sin(theta*np.pi/180)
+    cos_theta = np.cos(theta*np.pi/180)
+    v_new = v*cos_theta + np.cross(u,v)*sin_theta + np.dot(u,v)*u*(1-cos_theta)
+    return v_new
+'''
